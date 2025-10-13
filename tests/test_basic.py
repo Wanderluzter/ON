c08dc2054036da0c62307e4eda73fe7c1a9df3b7
Wanderluzter <leobotelho27@outlook.com>
2025-10-13 19:59:03 -0300

    Updated test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -10,12 +10,9 @@ def test_register_user():
         "idade": 25,
         "senha": "123456"
     }
-    
+
     response = client.post("/api/v1/auth/register", json=payload)
-    
-    # Testa se criou corretamente
     assert response.status_code == 201
     data = response.json()
     assert "id" in data
-    assert data["nome"] == payload["nome"]
-    assert data["email"] == payload["email"]
+
