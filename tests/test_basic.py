7f79a5283399d7e1539971694d74e81cfa7514af
Wanderluzter <leobotelho27@outlook.com>
2025-10-13 20:01:27 -0300

    Updated test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -12,7 +12,9 @@ def test_register_user():
     }
 
     response = client.post("/api/v1/auth/register", json=payload)
+
+    # Testa se criou corretamente
     assert response.status_code == 201
     data = response.json()
-    assert "id" in data
+    assert "id" in data  # só podemos checar o 'id', que é o que o endpoint retorna
 
